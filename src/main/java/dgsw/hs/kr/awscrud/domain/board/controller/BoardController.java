4691a3c2781bb71dd82ba6735cb2a9c44614c30a
김건오 <gokim0207@dgsw.hs.kr>
2026-07-27 16:03:47 +0900

    Use @RequestPart for multipart board creation request

--- src/main/java/dgsw/hs/kr/awscrud/domain/board/controller/BoardController.java
+++ src/main/java/dgsw/hs/kr/awscrud/domain/board/controller/BoardController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,7 +25,7 @@ public class BoardController {
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public BoardResponse createBoard(
-            @Valid @ModelAttribute BoardCreateRequest request,
+            @Valid @RequestPart("request") BoardCreateRequest request,
             @RequestPart(value = "image", required = false) MultipartFile image,
             @AuthenticationPrincipal AuthDetails authDetails
     ) {
